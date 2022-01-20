9455ee5e9b85ee3e572a0e3672ba382583e24389
DaveM-135 <67597482+DaveM-135@users.noreply.github.com>
2022-01-20 11:55:09 -0600

    Update EXEC_ETL_PSophitech.bat

--- Proyectos_Sophitech_Pentaho/Archivo bat/EXEC_ETL_PSophitech.bat
+++ Proyectos_Sophitech_Pentaho/Archivo bat/EXEC_ETL_PSophitech.bat
@@ -1,5 +1,5 @@
 cd "C:\data-integration"
 echo Empieza ejecucion de ETL
-call Kitchen.bat /file:"C:\Proyectos_Sophitech_Pentaho\PSophitech_Pentaho_II.kjb"
+call Kitchen.bat /file:"C:\Users\HP\Documents\GitHub\SophiETL\Proyectos_Sophitech_Pentaho\PSophitech_Pentaho_II.kjb"
 echo Termina ejecucion de ETL
 exit
